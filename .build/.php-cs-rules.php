5707cb062dbe503d0d21826fbc6426a333afd3b0
NarkNiro <markhouben91@gmail.com>
2022-02-28 09:23:27 +0100

    [TASK] Remove old comments from php-cs-fixer configuration

--- .build/.php-cs-rules.php
+++ .build/.php-cs-rules.php
@@ -1,26 +1,6 @@
 <?php
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
 /**
- * This file represents the configuration for Code Sniffing PSR-2-related
- * automatic checks of coding guidelines
- * Install @fabpot's great php-cs-fixer tool via
- *
- *  $ composer global require friendsofphp/php-cs-fixer
- *
- * And then simply run
- *
  *  $ php-cs-fixer fix --config ../.build/.php-cs-rules.php
  *
  * inside the TYPO3 directory. Warning: This may take up to 10 minutes.
@@ -32,20 +12,10 @@
 if (PHP_SAPI !== 'cli') {
     die('This script supports command line usage only. Please check your command.');
 }
-// Define in which folders to search and which folders to exclude
-// Exclude some directories that are excluded by Git anyways to speed up the sniffing
+
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__ . '/../packages/');
 
-// Return a Code Sniffing configuration using
-// all sniffers needed for PSR-2
-// and additionally:
-//  - Remove leading slashes in use clauses.
-//  - PHP single-line arrays should not have trailing comma.
-//  - Single-line whitespace before closing semicolon are prohibited.
-//  - Remove unused use statements in the PHP source code
-//  - Ensure Concatenation to have at least one whitespace around
-//  - Remove trailing whitespace at the end of blank lines.
 return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
